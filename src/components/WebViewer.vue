44aeeb730febf0ba3f81f96949cb67053d5a530d
unknown <ayip@pdftron.com>
2019-11-13 12:35:21 -0800

    small changes

--- src/components/WebViewer.vue
+++ src/components/WebViewer.vue
@@ -8,16 +8,57 @@ export default {
   name: 'WebViewer',
   props: {
     path: String,
-    url: String
+    url: String,
+    viewer_object: Object,
+    colorArr: Array,
+    layerArr: Array
   },
   mounted: function () {
-        WebViewer({
-            path: this.path,
-            initialDoc: this.url, // replace with your own PDF file
-          }, this.$refs.viewer).then((instance) => {
-            // call apis here
+    let _color = this.colorArr;
+    let _layer = this.layerArr;
+    let colorArr = {}; //this.$router.options.routes.find((data) => data.name == "color");
+    let layerArr = {}; //this.$router.options.routes.find((data) => data.name == "layer");
+    let vueObject = this;
+
+    WebViewer({
+        path: this.path,
+        fullAPI: true,
+        backendType: 'ems',
+        initialDoc: this.url, // replace with your own PDF fille
+      }, this.$refs.viewer).then((instance) => {
+        // call apis here
+        colorArr.viewerInstance = instance;
+        layerArr.viewerInstance = instance;
+        const docViewer = instance.docViewer;
+        docViewer.on('documentLoaded', () => {
+          const doc = docViewer.getDocument();
+    
+          doc.enableColorSeparations(true);
+          doc.on('colorSeparationAdded', (event, colorData) => {
+            if(!_color)
+              _color = [];
+            colorData.isSpot = colorData.color && colorData.color.filter((data) => data != 0).length == 1;
+            colorData.enable_default = true;
+            colorData._color = 'rgb(' + colorData.rgb.toString() + ')';
+            _color.push(colorData);
+
+            if(colorArr)
+              colorArr.params = {colorData : _color};
+          }); 
+
+          doc.getLayersArray().then((layersArray) => {
+            if(!_layer)
+              _layer = [];
+            _layer.every((data) => data.enable_default = true);
+            _layer = layersArray;
+            if(layerArr)
+              layerArr.params = {layerData : _layer}
           });
-    }
+        });
+
+        instance.setTheme('dark');
+      });
+  }
 }
 </script>
 
